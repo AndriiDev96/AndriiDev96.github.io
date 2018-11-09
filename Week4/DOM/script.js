63c93c2ee2549b79e7c89e2358e300dcf2895337
Andrii <33106099+AndriiDev96@users.noreply.github.com>
2018-11-09 11:57:05 +0200

    Add new homework week4(DOM)

---
+++ Week4/DOM/script.js
@@ -0,0 +1,65 @@
+const mainDiv = document.querySelector('.main-div-block');
+const centerBtn = document.querySelector('.section-button');
+const body = document.querySelector('body');
+const buttonBlue = document.querySelectorAll('.btn')[0];
+const buttonPurp = document.querySelectorAll('.btn')[1];
+
+//SetTimeout
+var i = 0;
+
+//Style
+body.setAttribute("style", `margin-left: auto; margin-right: auto; width: 70%; background-color: #ececec`);
+
+centerBtn.setAttribute("style", `display: flex; justify-content: center; 
+                                margin-top: 50px; margin-bottom: 50px;`);
+
+buttonBlue.setAttribute("style", `border: none; background-color: #16abf3; 
+                                  padding: 8px 14px 8px 14px; margin-left: 10px; color: white;`);
+
+buttonPurp.setAttribute("style", `border: none; background-color: #a038ff; 
+                                  padding: 8px 14px 8px 14px; margin-left: 10px; color: white;`);
+
+//Random color                           
+function backgroundDiv(){
+  var symb,
+      color;
+  symb = "0123456789ABCDEF";
+  color = "#";
+  for(var i = 0; i < 6; i++){
+    color = color + symb[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+//Function show divs
+const callback = (function(){
+  function createDiv(){
+    var newDivs = document.createElement('div');
+    var numText = document.createElement('p');
+    newDivs.setAttribute("style", `height: 115px; width: 115px; float: left;
+                              margin-left: 18px; margin-bottom: 30px; 
+                              background-color: ${backgroundDiv()};`);
+    numText.setAttribute("style", `text-align: center; padding-top: 30px; font-size: 20px;`);
+        
+    setTimeout(function(){
+      i++;
+      if(i <= 21){
+        numText.innerText = i;
+        mainDiv.appendChild(newDivs);
+        newDivs.appendChild(numText);
+        createDiv();
+      }else{
+        clearTimeout();
+      }
+    }, 500);
+  }
+  createDiv();
+  buttonBlue.removeEventListener('click', callback);
+});
+//Start
+buttonBlue.addEventListener('click', callback);
+//Button updade page
+buttonPurp.addEventListener('click', function(){
+  location.reload();
+  //mainDiv.innerHTML = "";
+});  
